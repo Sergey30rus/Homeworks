7a3b5a7ece0e102a414384a739990cacf7ec175d
Sergey Kolov <ifaith30rus@gmail.com>
2022-12-22 11:35:58 +0400

    added function addNumberOfChapters

--- index.js
+++ index.js
@@ -25,3 +25,28 @@ const user1 = {
   // Sample usage - do not modify
   getIntials(user1); // "SD"
   getIntials(user2); // "CF"
+
+  //task. Добавление чисел
+  const course1 = {
+    name: "Learn English",
+    isCompleted: true
+  };
+  
+  const course2 = {
+    name: "Learn JavaScript",
+    isCompleted: false
+  };
+  
+  /**
+   * @param {Object[]} course
+   * @param {number} count
+   */
+  function addNumberOfChapters(course, count) {
+    course.numberOfChapters = count
+    return course
+  }
+  
+  
+  // Sample usage - do not modify
+  addNumberOfChapters(course1, 80); // { name: 'Learn English', isCompleted: true, numberOfChapters: 80 }
+  addNumberOfChapters(course2, 60); // { name: 'Learn JavaScript', isCompleted: false, numberOfChapters: 60 }
