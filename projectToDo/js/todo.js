6ee73463f5e492333bfc768550d154ebdd1a66c0
Sergey Kolov <ifaith30rus@gmail.com>
2022-12-23 16:37:32 +0400

    added function createTodo in todo.js, added link on css in index.html

--- projectToDo/js/todo.js
+++ projectToDo/js/todo.js
@@ -4,5 +4,27 @@
  * @param {object} data
  */
 export function createTodo(data) {
-    
+    if(data.completed === true) {
+        return {
+            title: data.todoTitle,
+            isCompleted: data.completed,
+            icon: "✅",
+            category: data.chosenCategory,
+            author: {
+              firstName: data.user.first,
+              lastName: data.user.last
+            }
+          }
+    } else {
+        return {
+            title: data.todoTitle,
+            isCompleted: data.completed,
+            icon: "⏳",
+            category: data.chosenCategory,
+            author: {
+              firstName: data.user.first,
+              lastName: data.user.last
+            }
+        }
+    }
 }
