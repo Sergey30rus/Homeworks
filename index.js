62dcebe5b9caf1d5986d08cf2aa0d7d7cb9bc8e3
Sergey Kolov <ifaith30rus@gmail.com>
2022-12-23 15:06:14 +0400

    added function getFirstStudentGrade

--- index.js
+++ index.js
@@ -99,3 +99,36 @@ function getLastUserFullName(users) {
 
 // Sample usage - do not modify
 getLastUserFullName(users); // "Charlie Bon"
+
+const students = [
+  {
+    name: "Sam Doe",
+    age: 24,
+    learnEnglish: {
+      startYear: 2020,
+      grade: 18
+    }
+  },
+  {
+    name: "Charlie Bron",
+    age: 31,
+    learnEnglish: {
+      startYear: 2021,
+      grade: 19
+    }
+  }
+];
+
+/**
+ * @typedef {{startYear: Number, grade: Number}} Course
+ * @typedef {{name: String, age: Number, learnEnglish: Course}} Student
+ *
+ * @param {Student[]} students
+ */
+function getFirstStudentGrade(students) {
+	return students[0].learnEnglish.grade
+}
+
+
+// Sample usage - do not modify
+console.log(getFirstStudentGrade(students)); // 18
--- index1.js
+++ index1.js
@@ -1,9 +1,4 @@
-
-
-  
- 
-  
-  const students = [
+const students = [
     {
       name: "Sam Doe",
       age: 24,
@@ -29,43 +24,9 @@
    * @param {Student[]} students
    */
   function getFirstStudentGrade(students) {
-      return students[0].learnEnglish.grade
-  }
-  
-  
-  // Sample usage - do not modify
-  getFirstStudentGrade(students); // 18
-  
-  //task. Сумма вложенных оценок
-  const students = [
-    {
-      name: "Sam Doe",
-      age: 24,
-      learnEnglish: {
-        startYear: 2020,
-        grade: 18
-      }
-    },
-    {
-      name: "Charlie Bron",
-      age: 31,
-      learnEnglish: {
-        startYear: 2021,
-        grade: 19
-      }
-    }
-  ];
-  
-  /**
-   * @typedef {{startYear: Number, grade: Number}} Course
-   * @typedef {{name: String, age: Number, learnEnglish: Course}} Student
-   *
-   * @param {Student[]} students
-   */
-  function getSumGrades(students) {
       // your code
   }
   
   
   // Sample usage - do not modify
-  getSumGrades(students); // 37
+  getFirstStudentGrade(students); // 18
